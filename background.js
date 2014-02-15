55721570eeb18db31fb32870914c6b3d84fa31bb
sakib <sakib.jalal@gmail.com>
2014-02-15 18:11:59 -0500

    swag

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 //XML reader that returns javascript "entry" objects for gmail input
 
 function entry(title,summary,link,modified,issued,id,name,email) {
-	
 	this.email = email;
 	this.name = name;
 	this.id = id;
@@ -10,20 +9,15 @@ function entry(title,summary,link,modified,issued,id,name,email) {
 	this.link = link;
 	this.summary = summary;
 	this.title = title;
-
 }
-
 function readXML() {
-
 	xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("GET","https://mail.google.com/mail/feed/atom",false);
 	xmlhttp.send();
 	xmlDoc=xmlhttp.responseXML;
 	//xmlText=xmlhttp.responseText;
-
 	var entries = new Array();
 	var ent = xmlDoc.getElementsByTagName("entry");
-	
 	for (var i=0; i<ent.length; i++) {
 		var ne = new entry();
 		ne.title = ent[i].getElementsByTagName("title")[0].innerHTML;
@@ -36,9 +30,6 @@ function readXML() {
 		ne.email = ent[i].getElementsByTagName("email")[0].innerHTML;
 		entries.push(ne);
 	}
-
 	return entries;
-
 }
-
 readXML();
