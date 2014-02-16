76d6df65e9f80100fe558db6c2c2c5f7fce2db85
Belal Said <beloskhalil@gmail.com>
2014-02-15 21:52:51 -0500

    shity shit shit

--- insertionCode.js
+++ insertionCode.js
@@ -32,29 +32,51 @@ function readXML() {
 		ne.email = ent[i].getElementsByTagName("email")[0].innerHTML;
 		entries.push(ne);
 	}
-	
-	console.log(entries);
 	return entries;
 
 }
 
-function toggleDD() {
 
-}
 
 var parent = document.getElementById('jewelContainer');
-parent.innerHTML = "<div class='fbJewel' id='fbEmailJewel'> <a class='jewelButton' aria-labelledby='emailsCountWrapper' href='#'> </a> <div style='background-color:#fff;border:1px solid #333;border-bottom:2px solid #293e6a;left:0;position:absolute;top:40px;width:330px;z-index:-1; left: -165px; height: auto; max-height: 360px;'> <div style='height: 20px; width: auto; font-weight: bold; padding-left: 5px; padding-top: 5px'>Email Inbox</div> <div id='emailDropDown' style='border-bottom:1px solid #293e6a;border-top:1px solid #293e6a;background-color:#fff;overflow-x:hidden; overflow-y: scroll;width:330px; height: auto; max-height: 320px;'> </div> <div style='height: 20px; width: auto; font-weight: bold; padding-left: 5px; padding-top: 5px'> <a>See All</a> </div> </div> </div>" + parent.innerHTML;
+parent.innerHTML = "<div class='fbJewel' id='fbEmailJewel'> <a class='jewelButton' id='emailButton' aria-labelledby='emailsCountWrapper' href='#'> </a> <div style='background-color:#fff;border:1px solid #333;border-bottom:2px solid #293e6a;left:0;position:absolute;top:40px;width:330px;z-index:-1; left: -1000000px; height: auto; max-height: 370px;' id='emailContainer'> <div style='height: 20px; width: auto; font-weight: bold; padding-left: 5px; padding-top: 5px'>Email Inbox</div> <div id='emailDropDown' style='border-bottom:1px solid #293e6a;border-top:1px solid #293e6a;background-color:#fff;overflow-x:hidden; overflow-y: auto;width:330px; height: auto; max-height: 320px;'> </div> <div style='height: 20px; width: auto; font-weight: bold; padding-left: 5px; padding-top: 5px'><center><a href='https://mail.google.com/mail/u/0/#inbox' target='_blank'>See All</a><center> </div> </div> </div>" + parent.innerHTML;
+var dd = document.getElementById('emailDropDown'), ec = document.getElementById('emailContainer');
+setInterval(function()
+{
 var emails = readXML();
-var dd = document.getElementById('emailDropDown');
 if(emails.length > 0)
 {
-dd.innerHTML = dd.innerHTML + "<ul><hr/><li>";
+dd.innerHTML = "<ul><hr/><li>";
 }
 for(var i = 0; i < emails.length; i++)
 {
-dd.innerHTML = dd.innerHTML + "<li class='semail'><div><div style='font-weight: bold'>" + emails[i].name + "</div><div>" + emails[i].title + "</div></div><li><hr/>";
+dd.innerHTML = dd.innerHTML + "<li class='semail'><div><div style='font-weight: bold'>" + emails[i].name + "</div><div style='overflow: hidden; word-wrap: break-word'>" + emails[i].title + "</div></div><li><hr/>";
 }
 if(emails.length > 0)
 {
 dd.innerHTML = dd.innerHTML + "</li></ul>";
+}
+else
+{
+dd.innerHTML = "<ul><hr/><li><li class='semail'><div><div style='font-weight: bold'></div><div>No New Messages</div></div><li><hr/></li></ul>";
+}
+}, 5000);
+var dcase = true;
+
+
+function toggleDD() {
+  
+  if(!dcase)
+  {
+  ec.style.left = "-10000px";
+  }
+  else
+  {
+  ec.style.left = "-165px";
+  }
+  dcase = !dcase;
+}
+document.getElementById('emailButton').onclick = function()
+{
+toggleDD();
 }
